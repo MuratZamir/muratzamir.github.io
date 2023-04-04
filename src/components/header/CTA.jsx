6e0a3698e49d60c6f97b22a53c6a8f877eba94f5
MuratZamir <zamirpy@gmail.com>
2023-04-04 18:34:39 -0400

    commit

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ import CV from '../../assets/website-cv.pdf'
 export const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} target="_blank" className='btn'>Download CV</a>
+        <a href={CV} target="_blank" rel="noopener noreferrer" className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
